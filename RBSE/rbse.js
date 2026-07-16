ee6bf7650f208682dfd3984f7bdc04de2b0db5c8
1984arunavaishnav-svg <1984arunavaishnav@gmail.com>
2026-07-16 15:46:53 +0530

    Update rbse.js

--- RBSE/rbse.js
+++ RBSE/rbse.js
@@ -1,1 +1,30 @@
+const classSelect = document.getElementById("classSelect");
+const subjectSelect = document.getElementById("subjectSelect");
 
+classSelect.addEventListener("change", function () {
+
+    subjectSelect.innerHTML = "";
+
+    const selectedClass = this.value;
+
+    if (selectedClass === "") {
+        subjectSelect.disabled = true;
+        subjectSelect.innerHTML = "<option>Select Class First</option>";
+        return;
+    }
+
+    subjectSelect.disabled = false;
+
+    const subjects = rbseData[selectedClass].subjects;
+
+    subjectSelect.innerHTML = "<option value=''>Choose Subject</option>";
+
+    subjects.forEach(subject => {
+
+        subjectSelect.innerHTML += `
+            <option>${subject}</option>
+        `;
+
+    });
+
+});
