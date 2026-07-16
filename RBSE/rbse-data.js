f059d9b7a4c5582e8eedd695fb3efd98c03141e2
1984arunavaishnav-svg <1984arunavaishnav@gmail.com>
2026-07-16 15:07:09 +0530

    Update rbse-data.js

--- RBSE/rbse-data.js
+++ RBSE/rbse-data.js
@@ -1,1 +1,121 @@
+const rbseData = {
+  "Class 1": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "EVS"
+    ]
+  },
 
+  "Class 2": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "EVS"
+    ]
+  },
+
+  "Class 3": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "EVS"
+    ]
+  },
+
+  "Class 4": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "EVS"
+    ]
+  },
+
+  "Class 5": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "EVS"
+    ]
+  },
+
+  "Class 6": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "Science",
+      "Social Science",
+      "Sanskrit"
+    ]
+  },
+
+  "Class 7": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "Science",
+      "Social Science",
+      "Sanskrit"
+    ]
+  },
+
+  "Class 8": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "Science",
+      "Social Science",
+      "Sanskrit"
+    ]
+  },
+
+  "Class 9": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "Science",
+      "Social Science",
+      "Sanskrit"
+    ]
+  },
+
+  "Class 10": {
+    subjects: [
+      "Hindi",
+      "English",
+      "Maths",
+      "Science",
+      "Social Science",
+      "Sanskrit"
+    ]
+  },
+
+  "Class 11": {
+    subjects: [
+      "Physics",
+      "Chemistry",
+      "Maths",
+      "Biology",
+      "English"
+    ]
+  },
+
+  "Class 12": {
+    subjects: [
+      "Physics",
+      "Chemistry",
+      "Maths",
+      "Biology",
+      "English"
+    ]
+  }
+};
