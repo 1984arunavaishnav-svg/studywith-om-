6b24e33469b375fa0cdaca1d37f4b55ea52f509b
1984arunavaishnav-svg <1984arunavaishnav@gmail.com>
2026-07-17 13:11:29 +0530

    sdfsgffsgsd

--- RBSE/rbse.js
+++ RBSE/rbse.js
@@ -153,11 +153,11 @@ subjectSelect.addEventListener("change", async function () {
 
     });
 
-});chapterSelect.addEventListener("change", function () {
+chapterSelect.addEventListener("change", function () {
 
     if (this.value === "") return;
 
     window.location.href =
         `material.html?chapterId=${this.value}`;
 
-});
+})
