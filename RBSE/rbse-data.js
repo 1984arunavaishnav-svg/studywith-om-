87c72aafbd677174f82b7bc5765a7acae4a0722c
1984arunavaishnav-svg <1984arunavaishnav@gmail.com>
2026-07-17 10:12:37 +0530

    vhkd

--- RBSE/rbse-data.js
+++ RBSE/rbse-data.js
@@ -1,171 +1,317 @@
 const rbseData = {
- "Class 1": {
-    subjects: {
-        "Hindi": {
-    "Chapter 1": {
-        lecture: "#",
-        notes: "#",
-        pdf: "#",
-        quiz: "#"
-    },
 
-    "Chapter 2": {
-        lecture: "#",
-        notes: "#",
-        pdf: "#",
-        quiz: "#"
-    },
+"Class 1": {
 
-    "Chapter 3": {
-        lecture: "#",
-        notes: "#",
-        pdf: "#",
-        quiz: "#"
-    }
-},
-        
-        "English": [
-            "Chapter 1",
-            "Chapter 2",
-            "Chapter 3"
-        ],
-        "Maths": [
-            "Chapter 1",
-            "Chapter 2",
-            "Chapter 3"
-        ],
-        "EVS": [
-            "Chapter 1",
-            "Chapter 2",
-            "Chapter 3"
-        ]
-    }
+subjects: {
+
+"Hindi": {
+
+"Chapter(1)-परिवार": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
 },
 
-  "Class 2": {
-    subjects: {
-        "Hindi": [
-            "Chapter 1",
-            "Chapter 2",
-            "Chapter 3"
-        ],
-        "English": [
-            "Chapter 1",
-            "Chapter 2",
-            "Chapter 3"
-        ],
-        "Maths": [
-            "Chapter 1",
-            "Chapter 2",
-            "Chapter 3"
-        ],
-        "EVS": [
-            "Chapter 1",
-            "Chapter 2",
-            "Chapter 3"
-        ]
-    }
+"Chapter(2)-दादा-दादी": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
 },
 
-  "Class 3": {
-    subjects: [
-      "Hindi",
-      "English",
-      "Maths",
-      "EVS"
-    ]
-  },
+"Chapter(3)-रीना का दिन": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
 
-  "Class 4": {
-    subjects: [
-      "Hindi",
-      "English",
-      "Maths",
-      "EVS"
-    ]
-  },
+"Chapter(4)-रानी भी": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
 
-  "Class 5": {
-    subjects: [
-      "Hindi",
-      "English",
-      "Maths",
-      "EVS"
-    ]
-  },
+"Chapter(5)-मुर्गा बोला कु कड  -क": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
 
-  "Class 6": {
-    subjects: [
-      "Hindi",
-      "English",
-      "Maths",
-      "Science",
-      "Social Science",
-      "Sanskrit"
-    ]
-  },
+"Chapter(6)-तीन साथी": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(7)-वाह, मेरे घोड़!": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
 
-  "Class 7": {
-    subjects: [
-      "Hindi",
-      "English",
-      "Maths",
-      "Science",
-      "Social Science",
-      "Sanskrit"
-    ]
-  },
+"Chapter(8)-": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
 
-  "Class 8": {
-    subjects: [
-      "Hindi",
-      "English",
-      "Maths",
-      "Science",
-      "Social Science",
-      "Sanskrit"
-    ]
-  },
+"Chapter(9)-आलू की सडक": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(10)-झूलम-झूली": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
 
-  "Class 9": {
-    subjects: [
-      "Hindi",
-      "English",
-      "Maths",
-      "Science",
-      "Social Science",
-      "Sanskrit"
-    ]
-  },
+"Chapter(11)-भुट्टे": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
 
-  "Class 10": {
-    subjects: [
-      "Hindi",
-      "English",
-      "Maths",
-      "Science",
-      "Social Science",
-      "Sanskrit"
-    ]
-  },
+"Chapter(12)-फूली रोटी": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(13)- मेला": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(14)-बरखा और मेघा": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(15)-होली": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(16)-जनमदिवस पर पेड लगाओ: {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(17)-हवा": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(18)-कितनी प्यारी है ये दनिया": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+"Chapter(19)-चाद का बच्च": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+},
 
-  "Class 11": {
-    subjects: [
-      "Physics",
-      "Chemistry",
-      "Maths",
-      "Biology",
-      "English"
-    ]
-  },
+"English": {
 
-  "Class 12": {
-    subjects: [
-      "Physics",
-      "Chemistry",
-      "Maths",
-      "Biology",
-      "English"
-    ]
-  }
+"Chapter(1)-Two Little Hands ": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(2)-Greetings": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(3)-Picture Time": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(4)-The Cap-seller and the Monkeys ": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(5)-A Farm": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(6)-Fun with Pictures": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(7)-The Food we Eat": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(8)-The Four Seasons": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(9)- Anandi’s Rainbow": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+},
+
+"Maths": {
+
+"Chapter(1)-Finding the Furry Cat! (Pre-number Concepts)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(2)-What is Long? What is Round? (Shapes)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(3)-Mango Treat (Numbers 1 to 9)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(4)-Making 10 (Numbers 10 to 20)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(5)-How Many? (Addition and Subtraction of Single Digit Numbers)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(6)-Vegetable Farm (Addition and Subtraction up to 20)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(7)-Lina’s Family (Measurement)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(8)-Fun with Numbers (Numbers 21 to 99)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(9)-Utsav (Patterns)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(10)-How do I Spend my Day? (Time)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(11)-How Many Times? (Multiplication)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(12)-How Much Can We Spend? (Money)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter(13)-So Many Toys (Data Handling)": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+},
+
+
+"EVS": {
+
+"Chapter 1": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+},
+
+"Chapter 2": {
+lecture:"#",
+notes:"#",
+pdf:"#",
+quiz:"#"
+}
+
+}
+
+}
+
+}
+
 };
