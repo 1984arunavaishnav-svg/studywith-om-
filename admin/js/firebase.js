9cc1b632b022e5e9d7879b036d1c89b1e87d0676
1984arunavaishnav-svg <1984arunavaishnav@gmail.com>
2026-07-20 00:39:37 +0530

    dfsdfsdff

--- admin/js/firebase.js
+++ admin/js/firebase.js
@@ -1,12 +1,91 @@
-// ======================================
-// StudyWithOm CMS
-// Firebase Connection
-// ======================================
+/*==========================================================
+ StudyWithOm CMS
+ File : firebase.js
+ Version : 1.0
+==========================================================*/
 
-import { db, auth } from "../../firebase/firebase-config.js";
+/*==========================================================
+ FIREBASE IMPORTS
+==========================================================*/
 
-export { db, auth };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-console.log("Firebase Connected ✅");
-console.log(db);
-console.log(auth);
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+
+/*==========================================================
+ FIREBASE CONFIG
+==========================================================*/
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDwlHpTGv1NP93DxTNbMx09Nnz3YIFEAm8",
+
+    authDomain: "studywithom.firebaseapp.com",
+
+    projectId:  "studywithom",
+
+    storageBucket: "studywithom.firebasestorage.app",
+
+    messagingSenderId: "745374680590",
+
+    appId: "1:745374680590:web:cba7fe263951045f881e5f",
+
+};
+
+
+
+/*==========================================================
+ INITIALIZE FIREBASE
+==========================================================*/
+
+const app = initializeApp(firebaseConfig);
+
+
+
+/*==========================================================
+ SERVICES
+==========================================================*/
+
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+
+
+/*==========================================================
+ CLOUDINARY CONFIG
+==========================================================*/
+
+const cloudinary = {
+
+    cloudName: "abzhlo3h",
+
+    uploadPreset: "studywithom"
+
+};
+
+
+
+/*==========================================================
+ EXPORTS
+==========================================================*/
+
+export {
+
+    app,
+
+    db,
+
+    auth,
+
+    cloudinary
+
+};
